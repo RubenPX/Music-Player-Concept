490def60b076655f45b81f15cee6ddba593a73ad
RubenPX <rubenmarchante@gmail.com>
2022-11-02 14:58:21 +0100

    ✨ Display album song list

--- WebApp/script.js
+++ WebApp/script.js
@@ -17,4 +17,18 @@ function setupAlbums() {
     [...document.querySelectorAll("#AlbumList > [AlbumID]")].map(DOMItem => {
         DOMItem.addEventListener("click", () => SetAlbum(DOMItem.getAttribute("AlbumID")))
     })
+
+    SetAlbum(0)
+}
+
+function SetAlbum(num) {
+    const album = albumInfos[num]
+    document.getElementById("SongList").innerHTML = `<h1>${album.title}</h1>`
+    document.getElementById("SongList").innerHTML += album.playlist.map((song, index) => {
+        return `<div class="song" SongID="${index}" albumTitle="${album.title}" songURL="${song.url}" IMGAlbum="${album.img}">
+        <img src="${album.img}" />
+        <div>${song.title}</div>
+        <div class="duration">${song.duration}</div>
+      </div>`
+    }).join(" ");
 }
