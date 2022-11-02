ac5aaa275706ffc107c34d6189c93b170a872255
RubenPX <rubenmarchante@gmail.com>
2022-11-02 14:57:06 +0100

    ✨ Display albums

--- WebApp/script.js
+++ WebApp/script.js
@@ -1,4 +1,20 @@
 async function main() {
     albumInfos = await fetch("playlist.json").then(async res => await res.json())
+    setupAlbums()
 }
 main()
+
+function setupAlbums() {
+    document.getElementById("AlbumList").innerHTML = ""
+
+    document.getElementById("AlbumList").innerHTML = albumInfos.map((album, index) => {
+        return `<div class="album" AlbumID="${index}">
+            <img src="${album.img}">
+            <div class="album-title">${album.title}</div>
+        </div>`;
+    }).join(" ");
+
+    [...document.querySelectorAll("#AlbumList > [AlbumID]")].map(DOMItem => {
+        DOMItem.addEventListener("click", () => SetAlbum(DOMItem.getAttribute("AlbumID")))
+    })
+}
